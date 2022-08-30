058ea7b544c8a425b8f1c4383583fb835d715cad
Texi Schaeffer <texirae.schaeffer@gmail.com>
2022-08-30 07:22:48 -0500

    completed task 3

--- index.js
+++ index.js
@@ -231,10 +231,10 @@ Use getArtistByIndex to do the following:
 
 🌟 EXAMPLE: if getArtistByIndex is invoked with the artists array and the number 0, it will return `the artist at index 0 is Amedeo Modigliani` */
 
-function getArtistByIndex(/*Your Code Here*/) {
-  /*Your Code Here*/
+function getArtistByIndex(array, number) {
+  return `the artist at index ${array[number].id} is ${array[number].name}`;
 }
-
+console.log(getArtistByIndex(artists, 10));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 4: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
